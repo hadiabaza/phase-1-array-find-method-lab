294acede3a8552eaa409c28e45234fafa5db4c83
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-08 17:08:29 -0400

    commit tree ba225931ef0eae498227604c8fde16969e025182

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(yearResults) {
+  let winningYearResult = yearResults.find(
+    (yearResult) => yearResult.result === "W"
+  );
+  if (winningYearResult === undefined) {
+    return undefined;
+  } else {
+    return winningYearResult.year;
+  }
+}
