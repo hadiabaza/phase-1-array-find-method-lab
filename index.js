b81e4a7808c77f8eec555517140c91d60c001e20
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-08 18:19:15 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(yearResults) {
+  let winningYearResult = yearResults.find(
+    (yearResult) => yearResult.result === "W"
+  );
 
+  if (winningYearResult === undefined) {
+    return undefined;
+  } else {
+    return winningYearResult.year;
+  }
+}
+
+// Find takes a callback function
+// the call back function takes in an element in the
+// array and should return a boolean if it is the element we are looking for
